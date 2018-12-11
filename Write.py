7b6cb19656ebfab449b5a31af6b96fdf1810103e
joyxfeng <joyxfeng@mit.edu>
2018-12-10 20:02:27 -0500

    fixed everything! works with blue and green now

--- Write.py
+++ Write.py
@@ -9,7 +9,7 @@ import random
 
 cap=cv2.VideoCapture(0)
 
-pts_red = deque(maxlen=64)
+pts_green = deque(maxlen=64)
 pts_blue = deque(maxlen=64)
 
 Lower_blue = np.array([110, 50, 50]) # set lower bound for color recognition
@@ -17,11 +17,11 @@ Upper_blue = np.array([130, 255, 255]) # set upper bound for color recognition
 
 # blue detection range actually ranges from dark blue to a light yellow - weird?
 
-Lower_red = np.array([50, 50, 110]) # TRYING TO DETECT RED - COLOR INTERVAL WRONG
-Upper_red = np.array([255, 255, 130]) # TRYING TO DETECT RED - COLOR INTERVAL WRONG
+Lower_green = np.array([70, 50, 50]) # TRYING TO DETECT green - COLOR INTERVAL WRONG
+Upper_green = np.array([100, 255, 255]) # TRYING TO DETECT green - COLOR INTERVAL WRONG
 
-# tried to reverse RGB and BGR -- with this reversal ranges from dark red to light cyan-ish?
-# if we don't get this figured out by tonight we should email bove and see what he makes of it?
+# tried to reverse RGB and BGR -- with this reversal ranges from dark green to light cyan-ish?
+# if we don't get this figugreen out by tonight we should email bove and see what he makes of it?
 
 lower = np.array([0,0,0]) # BLACK for black screen
 upper = np.array([0,0,0]) # BLACK for black screen
@@ -38,12 +38,12 @@ while True:
 	res=cv2.bitwise_and(img,img,mask=mask_blue)
 	cnts, heir=cv2.findContours(mask_blue.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
 
-	mask_red = cv2.inRange(hsv, Lower_red, Upper_red)
-	mask_red = cv2.erode(mask_red, kernel, iterations=1)
-	mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_OPEN, kernel)
-	mask_red = cv2.dilate(mask_red, kernel, iterations=2)
-	res_red=cv2.bitwise_and(img, img, mask=mask_red)
-	cnts_red, heir_red = cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+	mask_green = cv2.inRange(hsv, Lower_green, Upper_green)
+	mask_green = cv2.erode(mask_green, kernel, iterations=1)
+	mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
+	mask_green = cv2.dilate(mask_green, kernel, iterations=2)
+	res_green=cv2.bitwise_and(img, img, mask=mask_green)
+	cnts_green, heir_green = cv2.findContours(mask_green.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
 
 
 	draw=cv2.bitwise_and(img, img, mask=mask2)
@@ -58,7 +58,7 @@ while True:
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
  
-		if radius > 50: # can we make it smaller to see it on a larger frame?
+		if radius > 10: # can we make it smaller to see it on a larger frame?
 			cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
 			cv2.circle(img, center, 5, (0, 0, 255), -1)
 		
@@ -71,25 +71,26 @@ while True:
 		cv2.line(gray, pts_blue[i-1],pts_blue[i],(0,0,0),thick)
 		cv2.line(draw, pts_blue[i-1],pts_blue[i],(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)),thickness_color) #random colors
 
-# DETECT RED
-	if len(cnts_red) > 0:
-		c = max(cnts_red, key=cv2.contourArea)
+# DETECT green
+	center = None
+	if len(cnts_green) > 0:
+		c = max(cnts_green, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		if radius > 50:  # can we make it smaller to see it on a larger frame?
+		if radius > 10:  # can we make it smaller to see it on a larger frame?
 			cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 			cv2.circle(img, center, 5, (0, 0, 255), -1)
 
-	pts_red.appendleft(center)
-	for i in range(1, len(pts_red)):
-		if pts_red[i - 1] is None or pts_red[i] is None:
+	pts_green.appendleft(center)
+	for i in range(1, len(pts_green)):
+		if pts_green[i - 1] is None or pts_green[i] is None:
 			continue
-		thick = int(np.sqrt(len(pts_red) / float(i + 1)) * 2.5)
-		thickness_color = int(np.sqrt(len(pts_red) / float(i + 1)) * 2)
-		cv2.line(gray, pts_red[i - 1], pts_red[i], (0, 0, 0), thick)
-		cv2.line(draw, pts_red[i - 1], pts_red[i], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+		thick = int(np.sqrt(len(pts_green) / float(i + 1)) * 2.5)
+		thickness_color = int(np.sqrt(len(pts_green) / float(i + 1)) * 2)
+		cv2.line(gray, pts_green[i - 1], pts_green[i], (0, 0, 0), thick)
+		cv2.line(draw, pts_green[i - 1], pts_green[i], (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
 				 thickness_color)  # random colors
 
 	cv2.imshow("frame", img) # color image
